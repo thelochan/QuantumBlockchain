76d075bce2559cad6207f86454f41ed2f862c699
prod-mini-17 <prod-mini-17@prod-mini-17.localdomain>
2023-05-24 11:54:48 +1200

    Quantum Block viz

--- quantum_rng.py
+++ quantum_rng.py
@@ -4,61 +4,10 @@ from qiskit.quantum_info import Statevector
 import matplotlib.pyplot as plt
 import random
 
+# Quantum Blockchain Protocol
 
-# def advanced_quantum_random_number_generator():
-#     # Create a Quantum Circuit acting on four quantum registers
-#     circuit = QuantumCircuit(4, 4)
-    
-#     # Get the initial statevector (all qubits in state |0>)
-#     state = Statevector.from_instruction(circuit)
-    
-#     # Visualize the initial state of the qubits
-#     plot_bloch_multivector(state).show()
-    
-#     # Add a H gate on each qubit, putting these qubits in superposition
-#     circuit.h([0, 1, 2, 3])
-    
-#     # Get the statevector after applying the Hadamard gates
-#     state = Statevector.from_instruction(circuit)
-    
-#     # Visualize the state of the qubits after applying the Hadamard gate
-#     plot_bloch_multivector(state).show()
-    
-#     # Add a series of CNOT gates to create entanglement
-#     circuit.cx(0, 1)
-#     circuit.cx(1, 2)
-#     circuit.cx(2, 3)
-    
-#     # Measure the qubits in the standard basis
-#     circuit.measure([0, 1, 2, 3], [0, 1, 2, 3])
-
-#     # Use Aer's qasm_simulator
-#     simulator = Aer.get_backend('qasm_simulator')
-
-#     # Execute the circuit on the qasm simulator, running it 1000 times
-#     job = execute(circuit, simulator, shots=1000)
-
-#     # Grab the results from the job
-#     result = job.result()
-
-#     # Return the counts
-#     return result.get_counts(circuit)
-
-# # Call the function to generate a random number
-# random_number = advanced_quantum_random_number_generator()
-
-# # Print the random number
-# print(random_number)
-
-# # Generate the histogram plot
-# plot = plot_histogram(random_number)
-
-# # Display the plot
-# plt.show()
-
-# QKD protocol
-
-# Step 1: User1 prepares a sequence of qubits
+# Step 1: User1 (the sender) prepares a sequence of qubits
+# In a blockchain context, this could represent a new block to be added to the chain
 num_qubits = 100
 user1_bits = [random.randint(0, 1) for _ in range(num_qubits)]
 user1_bases = [random.randint(0, 1) for _ in range(num_qubits)]
@@ -72,8 +21,8 @@ for i in range(num_qubits):
     qc.barrier()
     circuits.append(qc)
 
-# Step 2: User1 sends these qubits to User2
-# (In our simulation, User2 will just measure the qubits in User1's circuits)
+# Step 2: User1 sends these qubits to User2 (the receiver)
+# In a blockchain context, this could represent broadcasting the new block to the network
 
 # Step 3: User2 measures each qubit in a random basis
 user2_bases = [random.randint(0, 1) for _ in range(num_qubits)]
@@ -83,14 +32,50 @@ for i in range(num_qubits):
     circuits[i].measure(0, 0)
 
 # Step 4: User1 and User2 share the basis information
-# (In our simulation, User2 already knows User1's basis because he has her circuits)
+# In a blockchain context, this could represent the consensus process, where the network agrees on the validity of the new block
 key = []
+matching_bases = []
 for i in range(num_qubits):
     if user1_bases[i] == user2_bases[i]:
         result = execute(circuits[i], Aer.get_backend('qasm_simulator')).result()
         counts = result.get_counts(circuits[i])
         measured_bit = int(max(counts, key=counts.get))
         key.append(measured_bit)
+        matching_bases.append(i)
 
-# Step 5: User1 and User2 now have a shared secret key
-print("Shared secret key:", key)
+# Step 5: Error checking
+# User1 and User2 compare a random subset of their bits to check for errors
+# If the error rate is too high, they abort the protocol
+# In a blockchain context, this could represent checking the block for fraudulent transactions
+num_check_bits = min(10, len(matching_bases))
+check_bits = random.sample(matching_bases, num_check_bits)
+error_rate = sum(user1_bits[i] != key[matching_bases.index(i)] for i in check_bits) / num_check_bits
+if error_rate > 0.1:
+    print("Error rate too high, aborting protocol")
+    exit()
+# Step 6: Privacy amplification
+# User1 and User2 use a hash function to reduce the length of their key and increase its security
+# In a blockchain context, this could represent the mining process, where a new block is hashed to produce a proof-of-work
+hash_function = lambda bits: sum(bits) % 2
+hashed_key = [hash_function(key[i:i+10]) for i in range(0, len(key), 10)]
+
+# User1 and User2 now have a shared secret key that can be used to encrypt and decrypt messages
+# In a blockchain context, this key could beused to sign the new block, proving that it came from the sender
+print("Shared secret key:", hashed_key)
+
+# Step 7: User1 signs the block with her private key
+# In a blockchain context, this could represent the sender signing the new block with her private key
+signed_block = [bit ^ key_bit for bit, key_bit in zip(user1_bits, hashed_key)]
+
+# Step 8: User2 verifies the signature using the shared secret key
+# In a blockchain context, this could represent the network verifying the signature on the new block
+verified_block = [bit ^ key_bit for bit, key_bit in zip(signed_block, hashed_key)]
+if verified_block != user1_bits:
+    print("Signature verification failed, aborting protocol")
+    exit()
+
+# Step 9: User2 adds the verified block to the blockchain
+# In a blockchain context, this could represent the network adding the new block to the blockchain
+blockchain = [verified_block]  # In a real implementation, this would append to the existing blockchain
+
+print("Blockchain:", blockchain)
